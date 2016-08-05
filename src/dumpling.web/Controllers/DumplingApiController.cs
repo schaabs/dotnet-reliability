69b20598b13f2eadc41220005381c109641edc8e
sschaab <sschaab@microsoft.com>
2016-08-05 15:33:50 -0700

    initial port of dumpling.web and dumpling.db to new dumpling solution.

---
+++ src/dumpling.web/Controllers/DumplingApiController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace dumpling.web.Controllers
+{
+    public class DumplingApiController : ApiController
+    {
+
+        public async Task GetDumplingManifest(int dumplingId)
+        {
+
+        }
+
+        public async Task<int> CreateDumpling(string origin, string displayName)
+        {
+
+        }
+
+        public async Task UploadDumpFile(int dumplingId)
+        {
+
+        }
+
+        public async Task UploadArtifact(int? dumplingId, string index = null, string format = null, string localpath = null)
+        {
+
+        }
+    }
+}
